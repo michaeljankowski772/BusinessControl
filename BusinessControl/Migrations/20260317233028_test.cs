9b77a5ee6cbbeee45902c96f586f00c13c30e844
Michal <michael.jankowski.772@gmail.com>
2026-03-18 00:32:42 +0100

    testing migrations and solution in new environment

---
+++ BusinessControl/Migrations/20260317233028_test.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BusinessControlService.Migrations
+{
+    /// <inheritdoc />
+    public partial class test : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "FieldAreaAccepted",
+                table: "FieldJobs",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "PricePerArea",
+                table: "FieldJobs",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "PricePerAreaAccepted",
+                table: "FieldJobs",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FieldAreaAccepted",
+                table: "FieldJobs");
+
+            migrationBuilder.DropColumn(
+                name: "PricePerArea",
+                table: "FieldJobs");
+
+            migrationBuilder.DropColumn(
+                name: "PricePerAreaAccepted",
+                table: "FieldJobs");
+        }
+    }
+}
